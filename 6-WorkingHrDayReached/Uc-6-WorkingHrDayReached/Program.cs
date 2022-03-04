dd0db416ac38f9d645c085b12f26d9ee1e7553ac
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-04 21:51:44 +0530

    Addee code for working hour and day reached

---
+++ 6-WorkingHrDayReached/Uc-6-WorkingHrDayReached/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Uc_6_WorkingHrDayReached
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constant
+            int FullTime = 1;
+            int PartTime = 2;
+            int SalaryPerHr = 20;
+            int MaxDayOfWorking = 20;
+            int TotalWorkingHr = 100;
+
+            //variable
+            int WorkingDay = 0;
+            int WorkingHour = 0;
+            int empHr=0;
+
+            //computation
+            void method1()
+            {
+                Random random = new Random();
+                int check = random.Next(0, 3);
+                if (check == FullTime)
+                {
+                    empHr = 8;
+                }
+                else if (check == PartTime)
+                {
+                    empHr = 4;
+                }
+                else
+                {
+                    empHr = 0;
+                }
+            }
+
+            while(WorkingDay<=MaxDayOfWorking||WorkingHour<=TotalWorkingHr)
+            {
+
+                WorkingDay++;
+                int  Hr=medhod1();
+
+                WorkingHour = WorkingHour + Hr;
+            }
+
+            int TotalSalary=WorkingHour* SalaryPerHr;
+            Console.WriteLine("Total salary of Employee: "+TotalSalary);
+        }
+    }
+}
